1b54dd92b4d8c6efcf9237e6039fcd018cc68b04
zhangyigang <821288707@qq.com>
2019-10-24 20:59:10 +0800

    音乐类型判断

--- test/TestRequest.py
+++ test/TestRequest.py
@@ -13,6 +13,41 @@ from spider.FormatData import FormatData
 from Result.SearchResult import SearchResult
 reload(sys)
 sys.setdefaultencoding('utf-8')
+import time
+# 如果执行的是func1，那么会返回一个func2的函数
+def func1(func2):
+    # 测试装饰器外层函数运行几次
+    time1 = time.time()
+    print "qwer"
+    # 外层函数从来没运行过
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        print "开始时间"+str(time1)
+        print start_time
+        return func2(*args,**kwargs)
+    return wrapper
+def outer(level):
+    def middle(func):
+        # 中层这一步将函数名装填进去
+        def wrapper(*args,**kwargs):
+            if level==1:
+                func()
+        # 到最后装饰器返回的是这个函数，里面的args，kwargs都是运行时复制过去的
+        # 等于说运行时直接运行这个函数
+        return wrapper
+
+    return middle
+# 等到都成功之后就将wrapper这个函数名复制给原函数
+def func2():
+    print "asdf"
+@outer(level=1)
+def test_dector():
+    # 为什么装饰器里的外层函数不执行呢
+    print "test1"
+@func1
+def test_dector2():
+    print "test_dector2"
+
 class TestRequest(unittest.TestCase):
     def setUp(self):
         pass
@@ -39,3 +74,18 @@ class TestRequest(unittest.TestCase):
         searchResult.set_result(result)
         type = searchResult.judge_type()
         print "结果"+str(type)
+
+    def test_clouse(self):
+        test_dector
+        # test_dector()
+        print test_dector.func_name
+        print test_dector.__name__
+        print test_dector.__closure__[0].cell_contents
+        print test_dector.__closure__[1].cell_contents
+
+        # test_dector2()
+        # time.sleep(0.2)
+        # test_dector()
+        # func1(func2)
+        # another = func1(func2)
+        # another()
